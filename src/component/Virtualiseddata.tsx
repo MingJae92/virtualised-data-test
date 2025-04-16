31afcc82a36ac9dd8b7b8b9bb2519dccf45ed6fc
Ming Chi <wong_mingchi@yahoo.co.uk>
2025-04-16 17:31:35 +0100

    Mock data fetch test passed

--- src/component/Virtualiseddata.tsx
+++ src/component/Virtualiseddata.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProductTypes from "../types/virtualisedlist.types";
 
 function Virtualiseddata() {
+  const [products, setProducts] = useState<ProductTypes[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const productFetch = async () => {
+      try {
+        const dataResponse = await axios.get(
+          "https://fakestoreapi.com/products"
+        );
+        console.log(dataResponse.data);
+        setProducts(dataResponse.data);
+      } catch (error) {
+        setError(true);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    productFetch();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading server data</div>;
+  }
+
   return (
-    <div>Virtualiseddata</div>
-  )
+    <div>
+      <h1>Virtualised Data</h1>
+      <div>
+        <h2>Product List:</h2>
+        {products.map((item) => (
+          <div key={item.id}>{item.description}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Virtualiseddata
+export default Virtualiseddata;
--- src/test/virtualised.test.tsx
+++ src/test/virtualised.test.tsx
@@ -1,10 +1,32 @@
-import { describe, test } from "vitest";
+import { describe,  vi, expect } from "vitest";
+
+
+import { render, screen, waitFor } from "@testing-library/react";
 import Virtualiseddata from "../component/Virtualiseddata";
-import {  render } from "@testing-library/react";
+import axios from "axios";
 
+vi.mock("axios");
 
-describe("Vitualised component", ()=>{
-    test("Data is fetched", async()=>{
-        render (<Virtualiseddata/>)
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("Virtualised data component", () => {
+  const mockProducts = [
+    { id: 1, description: "Product A" },
+    { id: 2, description: "Product B" },
+  ];
+
+  it("Display product after successful fetch", async () => {
+    mockedAxios.get.mockImplementation(() =>
+      Promise.resolve({ data: mockProducts })
+    );
+
+    render(<Virtualiseddata/>)
+
+    await waitFor(()=>{
+      expect(screen.getByText("Product A")).toBeInTheDocument()
+      expect(screen.getByText("Product B")).toBeInTheDocument()
     })
-})
+  });
+});
+
+
